8793baaac9397ac1fb13da75374cf81399eb15c1
Aleksandr Lukash <a.lukash.92@gmail.com>
2020-10-14 22:30:54 +0300

    Initial Commit

---
+++ SemaphoreSwiftUI/SemaphoreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SemaphoreSwiftUIApp.swift
+//  SemaphoreSwiftUI
+//
+//  Created by Aleksandr on 14.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SemaphoreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
